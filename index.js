d4dbb02d2e7a76ec916fd9749371b4517368c065
Artem <the_n01se@mail.ru>
2023-06-26 14:33:39 +0700

    nearly finish

---
+++ index.js
@@ -0,0 +1,19 @@
+
+document.getElementById('burger-menu').addEventListener('click', () => {
+  document.querySelector('.menu__list').classList.toggle('open');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('menu__list').addEventListener('click', function () {
+  document.querySelector('.menu__list').classList.toggle("open");
+})
