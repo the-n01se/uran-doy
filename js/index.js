16eacdc1e443d1ac2742bf07097237b926d289b9
Artem <the_n01se@mail.ru>
2023-07-03 22:49:50 +0700

    added whatsapp link

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-document.getElementById('burger-menu').addEventListener('click', () => {
-  document.querySelector('.menu__list').classList.toggle('open');
-  document.body.classList.toggle("disable-scrolling");
-})
-document.getElementById('menu__list').addEventListener('click', function () {
-  document.querySelector('.menu__list').classList.remove("open");
-  document.body.classList.remove("disable-scrolling");
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('burger-menu').addEventListener('click', () => {
+    document.querySelector('.menu__list').classList.toggle('open');
+    document.body.classList.toggle("disable-scrolling");
+  });
+  document.getElementById('menu__list').addEventListener('click', function () {
+    document.querySelector('.menu__list').classList.remove("open");
+    document.body.classList.remove("disable-scrolling");
+  });
 })
